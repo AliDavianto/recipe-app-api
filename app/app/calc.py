95e519cb5e6d426e5aba049345f6e100ed906687
AliDavianto <yee39607@gmail.com>
2024-01-21 10:33:38 +0700

    Adding core app and configuring database

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculator Functions
+"""
+
+def add(x,y):
+    """Add x + y and Return Results"""
+    return x + y
+
+def substract(x,y):
+    """substract - + y and Return Results"""
+    return x - y
---
+++ app/app/tests.py
@@ -0,0 +1,21 @@
+"""
+Sample Test    
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+class Calcfest(SimpleTestCase):
+    """test the calc Module"""
+    def test_add_numbers(self):
+        """Test adding numbers together"""
+        res= calc.add(5,6)
+        
+        self.assertEqual(res,11)
+        
+    def test_substract_numers(self):
+        """Test substract numbers together"""
+        res= calc.substract(15,10)
+        
+        self.assertEqual(res,5)        
