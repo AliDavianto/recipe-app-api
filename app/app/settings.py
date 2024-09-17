dc2328586cc4f72946ff05745d2517230376c5dc
AliDavianto <yee39607@gmail.com>
2024-09-17 16:46:12 +0700

    proxy error

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,12 +20,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+vw%!7(=l6fp8gbb-6%mu1c#e%hq)sp1+_ko7nt4v3ufez+o6i'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
 ALLOWED_HOSTS = []
+ALLOWED_HOSTS.extend(
+    filter(
+        None,
+        os.environ.get('ALLOWED_HOSTS', '').split(',')
+    )
+)
 
 
 # Application definition
---
+++ proxy/default.conf.tpl
@@ -0,0 +1,14 @@
+server{
+    listen${LISTEN_PORT}
+
+    location /static {
+        alias /vol/static;
+
+    }
+
+    location / {
+        uwsgi               ${APP_HOST}:${APP_PORT};
+        include             /ect/nginx/uwsgi_params;
+        client_max_body_size 10M;
+    }
+}
